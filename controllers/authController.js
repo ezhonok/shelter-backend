50656cd868745262dc230fe6fe91d1672058c881
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-06-03 11:59:49 -0500

    resgiter built and tested

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,9 +4,59 @@ const User			= require('../models/user')
 const bcrypt		= require('bcryptjs')
 
 
+//registration
+router.post('/register', async (req, res, next) => {
+	const password = req.body.password
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+	console.log(passwordHash, ' This is hashed password');
+	const userData = {}
+	userData.username = req.body.username
+	userData.password = req.body.password
+	userData.img = req.body.img
+	userData.preferredname = req.body.preferredname
+	userData.accomplishment = req.body.accomplishment
+	userData.favoriteperson = req.body.favoritePerson
+	console.log(userData, ' this is userData in registration route');
 
+	try {
+		const createdUser = await User.create(userData)
+		req.session.logged = true
+		req.session.userDataId = createdUser._id
+		req.session.username = req.body.username
+		console.log('registered successfully');
+		res.json({
+			status: 200,
+			data: 'registration successful',
+			userId: createdUser._id
+		})
 
+	} catch(err){
+		next(err)
+	}
+})
 
+//login
+
+router.post('/login', async (req, res, next) => {
+	try{
+		const foundUser = await User.findOne({'username': req.body.username})
+		console.log(foundUser, 'This is foundUser in login route');
+		if(foundUser) {
+			if(bcrypt.compareSync(req.body.password, foundUser.password) === true){
+				req.session.logged = true
+				req.session.userDataId = foundUser._id
+				console.log('logged in successfully');
+
+				res.json({
+					status: 200,
+					data: 'login successful'
+				})
+			}
+		}
+	} catch(err) {
+		next(err)
+	}
+})
 
 
 
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express 		= require('express')
 const app 			= express()
 const bodyParser 	= require('body-parser')
-
+const session 		= require('express-session')
 
 require ('dotenv').config()
 
 require ('./db/db')
 
-
+app.use(session({
+	secret: process.env.SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -17,6 +21,10 @@ const authController = require('./controllers/authController')
 
 
 app.use('/auth', authController)
+
+
+
+
 
 app.listen(process.env.PORT || 9000, () => {
 	console.log('rabotaem na port ' + process.env.PORT);
