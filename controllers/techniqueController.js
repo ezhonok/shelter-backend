8d2e4c617e4f4610dae41d4afec32c3916d42fbd
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-06-04 11:27:28 -0500

    get route for user-specific outside/home/anywhere techniques built and tested

--- controllers/techniqueController.js
+++ controllers/techniqueController.js
@@ -48,19 +48,69 @@ router.get('/all-techniques', async (req, res, next) => {
 })
 
 //user can see their own techniques
-router.get('/all-techniques', async (req, res, next) => {
+router.get('/user-techniques', async (req, res, next) => {
 	try {
-		const allTechniques = await Technique.find()
-
+		const foundUser = await User.findById(req.session.userDataId).populate('technique')
+		allUserTechniques = foundUser.technique
 		res.json({
 			status: 200,
-			data: allLogs
+			data: allUserTechniques
 		})
 	} catch(err){
 		next(err)
 	}
 })
 
+
+//user can see their own home techniques
+router.get('/user-techniques-home', async (req, res, next) => {
+	try {
+		const foundUser = await User.findById(req.session.userDataId).populate('technique')
+		console.log("Here is the found user in home techniques");
+		console.log(foundUser);
+		
+		const allHomeTechniques = []
+		const homeTechniques = foundUser.technique.map(technique => {
+
+		if (technique.environment === 'home') {
+			const foundTechnique = technique.description
+			allHomeTechniques.push(foundTechnique)
+			console.log("Found home technique", foundTechnique);
+		}
+		})
+		res.json({
+			status: 200,
+			data: allHomeTechniques
+		})
+	} catch(err){
+		next(err)
+	}
+})
+
+
+//user can see their own outside techniques
+router.get('/user-techniques-outside', async (req, res, next) => {
+	try {
+		const foundUser = await User.findById(req.session.userDataId).populate('technique')
+		
+		const allOutsideTechniques = []
+		const outsideTechniques = foundUser.technique.map(technique => {
+
+		if (technique.environment === 'outside') {
+			const foundTechnique = technique.description
+			allOutsideTechniques.push(foundTechnique)
+			console.log("Found outside technique", foundTechnique);
+			// allHomeTechniques.push(foundTechnique)
+		}
+		})
+		res.json({
+			status: 200,
+			data: allOutsideTechniques
+		})
+	} catch(err){
+		next(err)
+	}
+})
 
 
 module.exports = router
